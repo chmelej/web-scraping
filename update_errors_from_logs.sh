3191c2be48290ad1c6dc0594715c9db2b5eaabc8
Jan Chmelensky <chmelej@czela.net>
2026-07-17 12:35:28 +0200

    sync

--- update_errors_from_logs.sh
+++ update_errors_from_logs.sh
@@ -1,1 +1,5 @@
+# run_scraper.sh po sobe nechava hodne logu. v logu jsou zajimave informace o
+# chybach, takze tento skript je projde a prida do databaze. je to naivni
+# prepisuje se posledni hodnota.
+
 find logs -type f -name *log -exec uv run scripts/update_errors_from_log.py {} \;
